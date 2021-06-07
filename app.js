57de00566e50adb1fa87e1c951fe8436ea2f2a7e
Sudeep Patra <55493181+SudeepPatra3@users.noreply.github.com>
2021-06-07 13:23:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,46 +5,76 @@ new Vue({
       con: [],
       commentsToShow: 4,
       totalleg: 0,
+      url: 1,
     };
   },
   created() {
-    axios
-      .get(
-        "https://api.themoviedb.org/3/movie/top_rated?api_key=100f625c280a240f47f9afb2eaf717b1&language=en-US"
-      )
-      .then((response) => {
-        this.con = response.data.results;
-      })
-      .catch((err) => console.log(err));
+    this.loaddata();
   },
   mounted() {
     this.totalleg = this.con.length;
   },
-});
-
-new Vue({
-    el: "#app2",
-    data() {
-      return {
-        con: [],
-        commentsToShow: 4,
-        totalleg: 0,
-      };
-    },
-    created() {
+  methods: {
+    loaddata() {
       axios
         .get(
-          "https://api.themoviedb.org/3/movie/upcoming?api_key=100f625c280a240f47f9afb2eaf717b1&language=en-US"
+          `https://api.themoviedb.org/3/movie/top_rated?api_key=100f625c280a240f47f9afb2eaf717b1&language=en-US&page=${this.url}`
         )
         .then((response) => {
           this.con = response.data.results;
         })
         .catch((err) => console.log(err));
     },
-    mounted() {
-      this.totalleg = this.con.length;
+
+    next() {
+      this.url++;
+      this.loaddata();
     },
-  });
+    previous() {
+      this.url--;
+      this.loaddata();
+    },
+  },
+});
+
+new Vue({
+  el: "#app2",
+  data() {
+    return {
+      con: [],
+      commentsToShow: 4,
+      totalleg: 0,
+      url: 1,
+    };
+  },
+  created() {
+    this.loaddata();
+  },
+  mounted() {
+    this.totalleg = this.con.length;
+  },
+  methods: {
+    loaddata() {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/upcoming?api_key=100f625c280a240f47f9afb2eaf717b1&language=en-US&page=${this.url}`
+        )
+        .then((response) => {
+          this.con = response.data.results;
+        })
+        .catch((err) => console.log(err));
+    },
+
+    next() {
+      this.url++;
+      this.loaddata();
+    },
+    previous() {
+      this.url--;
+      this.loaddata();
+    },
+  },
+});
 
 // Truncate filter
 var filter = function (text, length, clamp) {
